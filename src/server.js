8f2a66df48baabac3d574561711bd6bbf9f19b85
yezii <1126yezy@gmail.com>
2021-12-21 21:54:14 +0900

    Frontend Setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+// pug로 view engine 설정
+app.set("view engine", "pug");
 
-app.listen(3000);
+// express에 template이 어디 있는지 지정
+app.set("views", __dirname + "/views");
+
+// 유저에게 파일을 공유
+app.use("/public", express.static(__dirname + "/public"));
+
+// home.pug를 render 해주는 route handler 생성
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`✨ Listening on http://localhost:3000`);
+
+app.listen(3000, handleListen);
